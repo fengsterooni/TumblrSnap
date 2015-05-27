4443f58ee73ff57d97db4986a0d72eca28eba1eb
Feng Guo <fengsterooni@gmail.com>
2015-05-27 15:27:52 -0700

    Taking photo from camera or from gallery.

--- app/src/main/java/com/codepath/apps/tumblrsnap/fragments/PhotosFragment.java
+++ app/src/main/java/com/codepath/apps/tumblrsnap/fragments/PhotosFragment.java
@@ -1,16 +1,5 @@
 package com.codepath.apps.tumblrsnap.fragments;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.Locale;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -36,29 +25,40 @@ import com.codepath.apps.tumblrsnap.activities.PreviewPhotoActivity;
 import com.codepath.apps.tumblrsnap.models.Photo;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
+
 public class PhotosFragment extends Fragment {
 	private static final int TAKE_PHOTO_CODE = 1;
 	private static final int PICK_PHOTO_CODE = 2;
 	private static final int CROP_PHOTO_CODE = 3;
 	private static final int POST_PHOTO_CODE = 4;
-	
+
 	private Uri photoUri;
 	private Bitmap photoBitmap;
-	
+
 	TumblrClient client;
 	ArrayList<Photo> photos;
 	PhotosAdapter photosAdapter;
 	ListView lvPhotos;
-	
-	@Override 
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_photos, container, false);
 		setHasOptionsMenu(true);
 		return view;
 	}
-	
-	
+
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
@@ -69,7 +69,7 @@ public class PhotosFragment extends Fragment {
 		lvPhotos = (ListView) getView().findViewById(R.id.lvPhotos);
 		lvPhotos.setAdapter(photosAdapter);
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
@@ -88,30 +88,39 @@ public class PhotosFragment extends Fragment {
 			case R.id.action_take_photo:
 			{
 				// Take the user to the camera app
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				photoUri = Uri.fromFile(getOutputMediaFile());
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri); // set the image file name
+				// Start the image capture intent to take photo
+				startActivityForResult(intent, TAKE_PHOTO_CODE);
 			}
 			break;
 			case R.id.action_use_existing:
 			{
 				// Take the user to the gallery app
+				Intent intent = new Intent(Intent.ACTION_PICK,
+						MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				// Bring up gallery to select a photo
+				startActivityForResult(intent, PICK_PHOTO_CODE);
 			}
 			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == Activity.RESULT_OK) {
 			if (requestCode == TAKE_PHOTO_CODE) {
 				// Extract the photo that was just taken by the camera
-				
+
 				// Call the method below to trigger the cropping
-				// cropPhoto(photoUri)
+				cropPhoto(photoUri);
 			} else if (requestCode == PICK_PHOTO_CODE) {
 				// Extract the photo that was just picked from the gallery
-				
+				photoUri = data.getData();
 				// Call the method below to trigger the cropping
-				// cropPhoto(photoUri)
+				cropPhoto(photoUri);
 			} else if (requestCode == CROP_PHOTO_CODE) {
 				photoBitmap = data.getParcelableExtra("data");
 				startPreviewPhotoActivity();
@@ -120,7 +129,7 @@ public class PhotosFragment extends Fragment {
 			}
 		}
 	}
-	
+
 	private void reloadPhotos() {
 		client.getTaggedPhotos(new JsonHttpResponseHandler() {
 			@Override
@@ -141,7 +150,7 @@ public class PhotosFragment extends Fragment {
 			}
 		});
 	}
-	
+
 	private void cropPhoto(Uri photoUri) {
 		//call the standard crop action intent (the user device may not support it)
 		Intent cropIntent = new Intent("com.android.camera.action.CROP");
@@ -160,7 +169,7 @@ public class PhotosFragment extends Fragment {
 		//start the activity - we handle returning in onActivityResult
 		startActivityForResult(cropIntent, CROP_PHOTO_CODE);
 	}
-	
+
 	private String getFileUri(Uri mediaStoreUri) {
 		String[] filePathColumn = { MediaStore.Images.Media.DATA };
         Cursor cursor = getActivity().getContentResolver().query(mediaStoreUri,
@@ -169,16 +178,16 @@ public class PhotosFragment extends Fragment {
         int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
         String fileUri = cursor.getString(columnIndex);
         cursor.close();
-        
+
         return fileUri;
 	}
-	
+
 	private void startPreviewPhotoActivity() {
 		Intent i = new Intent(getActivity(), PreviewPhotoActivity.class);
         i.putExtra("photo_bitmap", photoBitmap);
         startActivityForResult(i, POST_PHOTO_CODE);
 	}
-	
+
 	private static File getOutputMediaFile() {
 	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
 	              Environment.DIRECTORY_PICTURES), "tumblrsnap");
@@ -193,7 +202,7 @@ public class PhotosFragment extends Fragment {
 
 	    return mediaFile;
 	}
-	
+
 	// Loads feed of users photos and merges them with the tagged photos
 	// Used to avoid an API limitation where user photos arent returned in tagged
 	private void mergeUserPhotos() {
